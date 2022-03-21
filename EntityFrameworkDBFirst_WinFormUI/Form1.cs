83d8da88d434b6b9c031af07c197c8d472fc95ce
birkanvaranofficial@gmail.com <birkanvaranofficial@gmail.com>
2022-03-21 11:21:37 +0300

    Add project files.

---
+++ EntityFrameworkDBFirst_WinFormUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityFrameworkDBFrist_BLL.ViewModels;
+using EntityFrameworkDBFrist_BLL;
+
+namespace EntityFrameworkDBFirst_WinFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // GLOBAL ALAN
+        ProductManager myProductManager = new ProductManager();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form yüklenirken Grid'e product bilgileri gelsin.
+            GrideProtuctlariGetir();
+        }
+
+        private void GrideProtuctlariGetir()
+        {
+            dataGridView1.DataSource = myProductManager.TumUrunleriGetir();
+            dataGridView1.Columns["SupplierID"].Visible = false;
+            dataGridView1.Columns["CategoryID"].Visible = false;
+
+        }
+    }
+}
---
+++ EntityFrameworkDBFrist_BLL/ProductManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkDBFrist_BLL.ViewModels;
+using EntityFrameworkDBFirst_DAL;
+
+namespace EntityFrameworkDBFrist_BLL
+{
+    public class ProductManager
+    {
+        NORTHWNDEntities myDBContext = new NORTHWNDEntities();
+        public List<ProductViewModel> TumUrunleriGetir()
+        {
+            List<ProductViewModel> urunler = new List<ProductViewModel>();
+            var data = myDBContext.Products.ToList();
+
+            foreach (var item in data)
+            {
+                ProductViewModel p = new ProductViewModel()
+                {
+                    CategoryID = item.CategoryID,
+                    ProductID = item.ProductID,
+                    UnitPrice = item.UnitPrice,
+                    UnitsInStock = item.UnitsInStock,
+                    Discontinued = item.Discontinued,
+                    UnitsOnOrder = item.UnitsOnOrder,
+                    ReorderLevel = item.ReorderLevel,
+                    SupplierID = item.SupplierID,
+                    ProductName = item.ProductName,
+                    QuantityPerUnit=item.QuantityPerUnit
+                };
+
+                // CategoryName
+                // 1. Yol:
+                p.CategoryName = item.Category.CategoryName;
+
+                //2. Yol:
+                //p.CategoryName=myDBContext.Categories.FirstOrDefault(x=>x.CategoryID=item.CategoryID)?.CategoryName;
+
+
+
+                urunler.Add(p);
+            }
+            return urunler;
+
+        }
+
+    }
+}
